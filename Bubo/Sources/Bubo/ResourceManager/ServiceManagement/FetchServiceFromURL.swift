6e4f049d8b46f75b47a85e9dc1172c2174250bc3
Valentin Hartig <valentin.hartig@tum.de>
2020-06-12 12:39:41 +0200

    Intergrated git root finder into service refreshment process to deal with nested repositories. There is still a not so beautiful mechansim behin this. Refer to BAVALENTINHARTIG-94 for more information.

--- Bubo/Sources/Bubo/ResourceManager/ServiceManagement/FetchServiceFromURL.swift
+++ Bubo/Sources/Bubo/ResourceManager/ServiceManagement/FetchServiceFromURL.swift
@@ -9,24 +9,49 @@ extension ResourceManager {
     func fetchServiceFromURL(serviceURL: URL) -> Service? {
         let fileManager = FileManager.default
         let name = fileManager.displayName(atPath: serviceURL.path)
+        var gitHash = ""
+        var gitURL: URL? = nil
+        var gitRootURL: URL? = nil
+        
+        let files: [File] = fileCrawler(startURL: serviceURL)
+        
+        for file in files {
+            if file.fileName == "Package.swift" {
+                gitRootURL = file.fileURL.deletingPathExtension().deletingLastPathComponent()
+            }
+        }
+        
+        guard let gitRootURLresolved = gitRootURL else {
+            outputMessage(msg: "Can't resolve git root directory for URL \(serviceURL.path)")
+            return nil
+        }
     
         do {
-            let gitURLstring =
-                try shellOut(to: "git -C \(serviceURL.path) config --get remote.origin.url")
-            guard let gitURL = URL(string: gitURLstring) else {
-                errorMessage(msg:
-                    "Can't parse the git remote URL" +
-                    "\(gitURLstring) for service \(serviceURL.path)" +
-                    "into the URL format")
-                abortMessage(msg: "Service creation")
-                return nil
-            }
-            let files = fileCrawler(startURL: serviceURL)
-            return Service(name: name, url: serviceURL, gitURL: gitURL, files: files)
+            let gitURLstring = try shellOut(to: "git -C \(gitRootURLresolved.path) config --get remote.origin.url")
+            gitURL = URL(string: gitURLstring)
         } catch {
-            errorMessage(msg: "Can't read the git remote URL for \(serviceURL.path)")
+            errorMessage(msg: "Can't read the git remote URL for \(gitRootURLresolved.path)")
             abortMessage(msg: "Service creation")
             return nil
         }
+        
+        do {
+            gitHash = try shellOut(to: "git -C \(gitRootURLresolved.path) rev-parse HEAD")
+        } catch {
+            errorMessage(msg: "Can't read the git hash for \(gitRootURLresolved.path)")
+            abortMessage(msg: "Service creation")
+            return nil
+        }
+        
+        guard let gitURLresolved = gitURL else {
+            errorMessage(msg:
+                "Can't parse the git remote URL" +
+                " for service \(serviceURL.path)" +
+                " into the URL format")
+            abortMessage(msg: "Service creation")
+            return nil
+        }
+        
+        return Service(name: name, url: serviceURL, gitURL: gitURLresolved, currentGitHash: gitHash, currentBuildGitHash: nil, files: files)
     }
 }
