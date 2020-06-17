e0e49e43d25fa872da547391b8e53f4f39081aa3
Valentin Hartig <valentin.hartig@tum.de>
2020-06-17 19:51:57 +0200

    Reworked pull service functionality

--- Bubo/Sources/Bubo/ResourceManager/ServiceVCS/PullServices.swift
+++ Bubo/Sources/Bubo/ResourceManager/ServiceVCS/PullServices.swift
@@ -15,23 +15,16 @@ extension ResourceManager {
     
     func pullService(projectName: String?, serviceName: String) -> Void {
         
-        /// Get the `projecHandle` and the project configuration `projectConfig`
-        guard let (projectHandle, projectConfig) = self.decodeProjectConfig(pName: projectName) else {
+        /// Refresh the services of the project to make sure all services (also manually cloned) are registered and up to date
+        self.refreshServices(projectName: projectName)
+
+        /// Get the `projecHandle` and the service configuration `service`
+        guard let (projectHandle, service) = self.decodeServiceConfig(pName: projectName, serviceName: serviceName) else {
             abortMessage(msg: "Pull service \(serviceName)")
             return
         }
         
-        headerMessage(msg: "Pulling service \(serviceName) in \(projectHandle)")
-        
-        /// Refresh the services of the project to make sure all services (also manually cloned) are registered and up to date
-        self.refreshServices(projectName: projectHandle)
-        
-        /// Try to get the service from the projects services
-        let services = projectConfig.repositories
-        guard let service = services[serviceName] else {
-            errorMessage(msg: "No service named \(serviceName) exists in project \(projectHandle)")
-            return
-        }
+        headerMessage(msg: "Pulling service \(service.name) in \(projectHandle)")
         
         /// Change to service directory and try to pull changes
         if fileManager.changeCurrentDirectoryPath(service.url.path) {
@@ -74,22 +67,24 @@ extension ResourceManager {
         self.refreshServices(projectName: projectHandle)
         
         /// Iterate over all projects and try to pull changes
-        let services = projectConfig.repositories
-        for (serviceName,service) in services {
-            if fileManager.changeCurrentDirectoryPath(service.url.path) {
-                do {
-                    let output = try shellOut(to: "git pull")
-                    outputMessage(msg: output)
-                    outputMessage(msg: "Pulled \(serviceName) in \(projectHandle) successfully")
-                } catch {
-                    let error = error as! ShellOutError
-                    print(error.message) // Prints STDERR
-                    print(error.output) // Prints STDOUT
-                    errorMessage(msg: "Failed to pull project \(projectHandle)")
-                    return
+        for (serviceName, _) in projectConfig.repositories {
+            
+            if let (_,serviceConfig) = decodeServiceConfig(pName: projectHandle, serviceName: serviceName) {
+                if fileManager.changeCurrentDirectoryPath(serviceConfig.url.path) {
+                    do {
+                        let output = try shellOut(to: "git pull")
+                        outputMessage(msg: output)
+                        outputMessage(msg: "Pulled \(serviceConfig.name) in \(projectHandle) successfully")
+                    } catch {
+                        let error = error as! ShellOutError
+                        print(error.message) // Prints STDERR
+                        print(error.output) // Prints STDOUT
+                        errorMessage(msg: "Failed to pull project \(projectHandle)")
+                        return
+                    }
+                } else {
+                    errorMessage(msg: "Failed to open the direectory of service \(serviceConfig.name)")
                 }
-            } else {
-                errorMessage(msg: "Failed to open the direectory of service \(service.name)")
             }
         }
         
