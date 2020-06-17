b30a6666e6f61b323f4c44570b1fae8adf4de6d2
Valentin Hartig <valentin.hartig@tum.de>
2020-06-17 20:41:47 +0200

    Added graph persisting functionality to ServiceManager and restructured it

--- Bubo/Sources/Bubo/ServiceManager/ServiceManager.swift
+++ Bubo/Sources/Bubo/ServiceManager/ServiceManager.swift
@@ -19,13 +19,7 @@ class ServiceManager {
         self.service = service
         self.parser = Parser()
         
-        // Build service
-        if !ServiceManager.compareGitHash(service: service, pName: pName) {
-            ServiceManager.cleanUpService(service: service)
-            ServiceManager.buildService(service: service)
-        }
-        
-        // Parse service + build graph
+        // Parse service
         parser.parse(service: service)
         self.graphBuilder = GraphBuilder(tokens: parser.tokens, tokenExtensions: parser.tokenExtensions, service: service)
         
@@ -37,101 +31,22 @@ class ServiceManager {
         self.projectName = name
     }
     
-    
-    
-    private static func compareGitHash(service: ServiceConfiguration, pName: String?) -> Bool {
-        guard var (projectHandle, projectConfig) = self.resourceManager.decodeProjectConfig(pName: pName) else {
-            abortMessage(msg: "Refresh services")
-            return false
-        }
-        
-        guard let buildHash = service.currentBuildGitHash else {
-            // Update project configuration with new build hash
-            var services = projectConfig.repositories
-            let updatedService: ServiceConfiguration = ServiceConfiguration(
-                name: service.name,
-                url: service.url,
-                gitURL: service.gitRemoteURL,
-                currentGitHash: service.currentGitHash,
-                currentBuildGitHash: service.currentGitHash,
-                files: service.files)
-            services[updatedService.name] = updatedService
-            projectConfig.repositories = services
-            projectConfig.lastUpdated = Date().description(with: .current)
-            self.resourceManager.encodeProjectConfig(pName: projectHandle, configData: projectConfig)
-            return false
-        }
-        
-        if service.currentGitHash == buildHash {
-            return true
-        } else {
-            var services = projectConfig.repositories
-            let updatedService: ServiceConfiguration = ServiceConfiguration(
-                name: service.name,
-                url: service.url,
-                gitURL: service.gitRemoteURL,
-                currentGitHash: service.currentGitHash,
-                currentBuildGitHash: service.currentGitHash,
-                files: service.files)
-            services[updatedService.name] = updatedService
-            projectConfig.repositories = services
-            projectConfig.lastUpdated = Date().description(with: .current)
-            self.resourceManager.encodeProjectConfig(pName: projectHandle, configData: projectConfig)
-            successMessage(msg: "Reencoded projectconfig")
-            return false
-        }
-    }
-    
-    private static func buildService(service: ServiceConfiguration) -> Void {
-        outputMessage(msg: "Initialising building process...")
-        if let packageDotSwiftURL = service.packageDotSwift?.fileURL {
-            if ServiceManager.fileManager.changeCurrentDirectoryPath(
-                packageDotSwiftURL
-                    .deletingPathExtension()
-                    .deletingLastPathComponent()
-                    .path
-                ) {
-                do {
-                    try shellOut(to: .buildSwiftPackage())
-                    outputMessage(msg: "Build for service \(service.name) completed")
-                } catch {
-                    let error = error as! ShellOutError
-                    errorMessage(msg: "Failed to build \(service.name). Can't index service if it's not build")
-                    warningMessage(msg: error.message) // Prints STDERR
-                    warningMessage(msg: error.output) // Prints STDOUT
-                }
-            }
-        } else {
-            warningMessage(msg: "Couldn't locate package.swift and therefore not build \(service.name). Indexing is not possible and therefor no graph will be geneerated")
-        }
-    }
-    
-    private static func cleanUpService(service: ServiceConfiguration) -> Void {
-        outputMessage(msg: "Cleaning up service...")
-        if ServiceManager.fileManager.fileExists(atPath: service.gitRootURL.appendingPathComponent(".build").path) {
-            if ServiceManager.fileManager.changeCurrentDirectoryPath(service.gitRootURL.path) {
-                do {
-                    try ServiceManager.fileManager.removeItem(at: service.gitRootURL.appendingPathComponent(".build"))
-                    outputMessage(msg: "Removed .build directory from \(service.name)")
-                } catch {
-                    errorMessage(msg: "Failed to remove .build directory from \(service.name)")
-                }
-            }
-        }
-    }
+ 
     public func createDependencyGraph() -> DependencyGraph<Node>? {
-        graphBuilder.createDependencyGraph()
-        self.updateGraph()
+        
+        // Build service
+        if !self.compareGitHash() || service.graph == nil{
+            self.cleanUpService()
+            self.buildService()
+            self.graphBuilder.createDependencyGraph()
+            self.updateGraph()
+        } else {
+            self.graphBuilder.graph = service.graph!
+        }
         return graphBuilder.graph
     }
     
     private func updateGraph() -> Void {
-        guard var (projectHandle, projectConfig) = ServiceManager.resourceManager.decodeProjectConfig(pName: projectName) else {
-            abortMessage(msg: "Refresh services")
-            return
-        }
-        
-        var services = projectConfig.repositories
         var updatedService: ServiceConfiguration = ServiceConfiguration(
             name: service.name,
             url: service.url,
@@ -140,9 +55,75 @@ class ServiceManager {
             currentBuildGitHash: service.currentGitHash,
             files: service.files)
         updatedService.setGraph(graph: graphBuilder.graph)
-        services[updatedService.name] = updatedService
-        projectConfig.repositories = services
-        projectConfig.lastUpdated = Date().description(with: .current)
-        ServiceManager.resourceManager.encodeProjectConfig(pName: projectHandle, configData: projectConfig)
+        ServiceManager.resourceManager.encodeServiceConfig(pName: projectName, configData: updatedService)
     }
+    
+    
+     private func compareGitHash() -> Bool {
+         
+         guard let buildHash = service.currentBuildGitHash else {
+             // Update project configuration with new build hash
+             let updatedService: ServiceConfiguration = ServiceConfiguration(
+                 name: service.name,
+                 url: service.url,
+                 gitURL: service.gitRemoteURL,
+                 currentGitHash: service.currentGitHash,
+                 currentBuildGitHash: service.currentGitHash,
+                 files: service.files)
+             self.resourceManager.encodeServiceConfig(pName: projectName, configData: updatedService)
+             return false
+         }
+         
+         if service.currentGitHash == buildHash {
+             return true
+         } else {
+             let updatedService: ServiceConfiguration = ServiceConfiguration(
+                 name: service.name,
+                 url: service.url,
+                 gitURL: service.gitRemoteURL,
+                 currentGitHash: service.currentGitHash,
+                 currentBuildGitHash: service.currentGitHash,
+                 files: service.files)
+             self.resourceManager.encodeServiceConfig(pName: projectName, configData: updatedService)
+             return false
+         }
+     }
+     
+     private func buildService() -> Void {
+         outputMessage(msg: "Initialising building process...")
+         if let packageDotSwiftURL = service.packageDotSwift?.fileURL {
+             if ServiceManager.fileManager.changeCurrentDirectoryPath(
+                 packageDotSwiftURL
+                     .deletingPathExtension()
+                     .deletingLastPathComponent()
+                     .path
+                 ) {
+                 do {
+                     try shellOut(to: .buildSwiftPackage())
+                     outputMessage(msg: "Build for service \(service.name) completed")
+                 } catch {
+                     let error = error as! ShellOutError
+                     errorMessage(msg: "Failed to build \(service.name). Can't index service if it's not build")
+                     warningMessage(msg: error.message) // Prints STDERR
+                     warningMessage(msg: error.output) // Prints STDOUT
+                 }
+             }
+         } else {
+             warningMessage(msg: "Couldn't locate package.swift and therefore not build \(service.name). Indexing is not possible and therefor no graph will be geneerated")
+         }
+     }
+     
+     private func cleanUpService() -> Void {
+         outputMessage(msg: "Cleaning up service...")
+         if ServiceManager.fileManager.fileExists(atPath: service.gitRootURL.appendingPathComponent(".build").path) {
+             if ServiceManager.fileManager.changeCurrentDirectoryPath(service.gitRootURL.path) {
+                 do {
+                     try ServiceManager.fileManager.removeItem(at: service.gitRootURL.appendingPathComponent(".build"))
+                     outputMessage(msg: "Removed .build directory from \(service.name)")
+                 } catch {
+                     errorMessage(msg: "Failed to remove .build directory from \(service.name)")
+                 }
+             }
+         }
+     }
 }
