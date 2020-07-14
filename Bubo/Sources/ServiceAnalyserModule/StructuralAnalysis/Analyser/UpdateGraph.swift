786e0bfd834b70e4a835a8d8c1622f8eb557658b
Valentin Hartig <valentin.hartig@tum.de>
2020-07-14 12:51:35 +0200

    Reworked the updateGraph() method so it's not generating undefined behaviour if used for other purposes

--- Bubo/Sources/ServiceAnalyserModule/StructuralAnalysis/Analyser/UpdateGraph.swift
+++ Bubo/Sources/ServiceAnalyserModule/StructuralAnalysis/Analyser/UpdateGraph.swift
@@ -8,12 +8,20 @@ import ResourceManagerModule
 
 extension StructuralAnalyser {
     public func updateGraph() -> Void {
-        let graphsnapshot: GraphSnapshot = GraphSnapshot(timestamp: Date().description(with: .current), buildGitHash: service.currentGitHash, graph: graphBuilder!.graph)
+        guard let gb = self.graphBuilder else {
+            errorMessage(msg: "Can't access graphbuilder because it hasn't been initialised!")
+            return
+        }
+        
+        let graphsnapshot: GraphSnapshot = GraphSnapshot(timestamp: Date().description(with: .current), buildGitHash: service.currentGitHash, graph: gb.graph)
         
         if let url = self.resourceManager.encodeGraphSnapshot(pName: projectName, serviceName: service.name, graphSnapshot: graphsnapshot) {
-            self.service.addGraphSnapshot(url: url)
-            self.resourceManager.encodeServiceConfig(pName: projectName, configData: self.service)
-            self.mostRecentGraphSnapshot = graphsnapshot
+            if self.resourceManager.encodeServiceConfig(pName: projectName, configData: self.service) != nil {
+                self.service.addGraphSnapshot(url: url)
+                self.mostRecentGraphSnapshot = graphsnapshot
+            } else {
+                abortMessage(msg: "Graph snapshot creation")
+            }
         } else {
             errorMessage(msg: "Failed to update service: No url for encoded snapshot returned!")
         }
